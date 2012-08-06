6239a09e960d650f7c9c70f64d4981bc923ff3ec
Josh Watzman <jwatzman@jwatzman.org>
2012-08-05 19:58:38 -0700

    Make login a valid HTML page

--- Page/Login.hs
+++ Page/Login.hs
@@ -5,6 +5,9 @@ import qualified Text.Blaze.Html5 as H
 
 import qualified Facebook.Config
 import qualified Template.Login
+import qualified Template.Page
 
 render :: S.ServerPartT IO H.Html
-render = S.ok $ Template.Login.render Facebook.Config.appid
+render = S.ok $
+	Template.Page.render "Log In" $
+		Template.Login.render Facebook.Config.appid
