6bde1b8da4583fc352ed58f15913fd1378476434
Josh Watzman <jwatzman@jwatzman.org>
2013-06-11 22:27:19 -0700

    Switch to aeson for JSON

--- Facebook/Cookie.hs
+++ Facebook/Cookie.hs
@@ -1,18 +1,16 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Facebook.Cookie(fbidFromCookie) where
 
 import qualified Codec.Binary.Base64Url as B64
 import Codec.Utils (Octet)
-import qualified Data.ByteString.Internal as BS
-import Data.Generics (Data)
-import qualified Data.HMAC as HMAC
+import qualified Data.Aeson as AE
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Internal as BSI
 import qualified Data.Digest.SHA256 as SHA256
-import Data.Typeable (Typeable)
+import qualified Data.HMAC as HMAC
 import Data.Word (Word8)
-import qualified Text.JSON as JSON
-import qualified Text.JSON.Generic as JSONG
-import qualified Text.JSON.String as JSONS
+import GHC.Generics (Generic)
 
 import qualified Facebook.Config
 import Facebook.FBID
@@ -20,7 +18,8 @@ import Facebook.FBID
 data FBJson = FBJson {
 	algorithm :: String,
 	user_id :: String
-} deriving (Eq, Show, Data, Typeable)
+} deriving (Generic)
+instance AE.FromJSON FBJson
 
 hmac_sha256 :: [Octet] -> [Octet] -> [Octet]
 hmac_sha256 = HMAC.hmac $ HMAC.HashMethod SHA256.hash 512
@@ -46,13 +45,10 @@ decodeB64 encoded =
 		Just l -> return l
 		Nothing -> fail "Failed to deocde base64"
 
-decodeFBJson :: (Monad m) => String -> m FBJson
-decodeFBJson json =
-	case JSONS.runGetJSON JSONS.readJSValue json of
-		Left s -> fail s
-		Right j -> case JSONG.fromJSON j of
-			JSON.Error s -> fail s
-			JSON.Ok x -> return x
+decodeFBJson :: (Monad m) => BSL.ByteString -> m FBJson
+decodeFBJson json = case AE.eitherDecode json of
+	Left err -> fail err
+	Right result -> return result
 
 safeRead :: (Read r, Monad m) => String -> m r
 safeRead s = case reads s of
@@ -64,15 +60,15 @@ fbidFromCookie cookie = do
 	let (signatureEncoded, jsonEncoded) = splitCookie cookie
 	signature <- decodeB64 signatureEncoded
 	json <- decodeB64 jsonEncoded
-	fbjson <- decodeFBJson $ map BS.w2c json
+	fbjson <- decodeFBJson $ BSL.pack json
 	if algorithm fbjson == "HMAC-SHA256" then
 		return ()
 	else
 		fail $ "Invalid algorithm " ++ algorithm fbjson
 	let signatureExpected =
 		hmac_sha256
-			(map BS.c2w Facebook.Config.appsecret)
-			(map BS.c2w jsonEncoded)
+			(map BSI.c2w Facebook.Config.appsecret)
+			(map BSI.c2w jsonEncoded)
 	if signature == signatureExpected then
 		return ()
 	else
