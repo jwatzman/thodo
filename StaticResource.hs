17af3c234da690226cb41d0c64a8e2907f72e1b9
Josh Watzman <jwatzman@jwatzman.org>
2013-06-11 22:31:36 -0700

    Import SR from bethanywasright

--- StaticResource.hs
+++ StaticResource.hs
@@ -1,23 +1,69 @@
-module StaticResource(addCss, addJs, runSR, StaticResource) where
+{-# LANGUAGE GeneralizedNewtypeDeriving, OverloadedStrings, RecordWildCards #-}
+
+module StaticResource(
+	StaticResource,
+	SRResult(..),
+	addCss,
+	addJs,
+	addMeta,
+	addSigil,
+	runSR
+) where
 
 import Control.Monad.State
+import qualified Data.Set as S
+import Text.Blaze ((!))
+import qualified Text.Blaze as B
+import qualified Text.Blaze.Html5 as H
 
-type SRPair = ([String], [String])
-newtype StaticResource a = SR { getSR :: State SRPair a }
+data SRData = SRData {
+	cssSet :: S.Set String,
+	jsSet :: S.Set String,
+	metaPrefix :: String,
+	nextMeta :: Int,
+	reversedMeta :: [String] -- TODO this should really be some sort of map
+}
 
-instance Monad StaticResource where
-	return = SR . return
-	(SR a) >>= bgen = SR $ a >>= (getSR . bgen)
+data SRResult = SRResult { css :: [String], js :: [String], meta :: [String] }
+newtype StaticResource a = SR { getSR :: State SRData a }
+	deriving (Monad)
 
 addCss :: String -> StaticResource ()
 addCss newCss = do
-	(css, js) <- SR get
-	SR $ put (newCss:css, js)
+	sr@SRData{..} <- SR get
+	SR $ put $ sr { cssSet = S.insert newCss cssSet }
 
 addJs :: String -> StaticResource ()
 addJs newJs = do
-	(css, js) <- SR get
-	SR $ put (css, newJs:js)
+	sr@SRData{..} <- SR get
+	SR $ put $ sr { jsSet = S.insert newJs jsSet }
 
-runSR :: StaticResource a -> (a, SRPair)
-runSR sr = runState (getSR sr) ([], [])
+addMeta :: String -> H.Html -> StaticResource H.Html
+addMeta meta h = do
+	sr@SRData{..} <- SR get
+	SR $ put $ sr { nextMeta = succ nextMeta, reversedMeta = meta:reversedMeta }
+	return $
+		h ! (B.dataAttribute "meta" $ H.toValue $ metaPrefix ++ (show nextMeta))
+
+addSigil :: String -> H.Html -> H.Html
+addSigil sigil h = h ! (B.dataAttribute "sigil" $ H.toValue sigil)
+
+initSRData :: Integer -> SRData
+initSRData metablock = SRData {
+	cssSet = S.empty,
+	jsSet = S.empty,
+	metaPrefix = (show metablock) ++ "_",
+	nextMeta = 0,
+	reversedMeta = []
+}
+
+runSR :: Integer -> StaticResource a -> (a, SRResult)
+runSR metablock sr =
+	let
+		(r, SRData{..}) = runState (getSR sr) (initSRData metablock)
+		result = SRResult {
+			css = S.toList cssSet,
+			js = S.toList jsSet,
+			meta = reverse reversedMeta
+		}
+	in (r, result)
--- Template/Page.hs
+++ Template/Page.hs
@@ -1,8 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards, OverloadedStrings #-}
 
-module Template.Page(render) where
+module Template.Page(render, prependPath) where
 
-import Control.Monad (mapM_)
 import Text.Blaze ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
@@ -11,14 +10,33 @@ import qualified StaticResource as SR
 
 render :: String -> SR.StaticResource H.Html -> H.Html
 render title body =
-	let (bodyMarkup, (css, js)) = SR.runSR body in
+	let (bodyMarkup, SR.SRResult{..}) = SR.runSR 0 body in
 	H.docTypeHtml $ do
 		H.head $ do
 			H.title $ H.toHtml title
+			H.meta ! A.name "viewport" ! A.content
+				"width=device-width, initial-scale=1, maximum-scale=1"
+			renderJs "javelin.js"
+			renderMeta meta
 			mapM_ renderJs js
-			-- TODO: render CSS
+			renderCss "Page.css"
+			mapM_ renderCss css
+			-- NB: the order is important here -- resource needs to be last
+			-- so it can pick up on all the other files we've loaded
+			renderJs "javelin-resource.js"
 		H.body $ do
 			bodyMarkup
 
+prependPath :: String -> String
+prependPath = (++) "/static/"
+
 renderJs :: String -> H.Html
-renderJs js = H.script ! (A.src $ H.toValue js) $ ""
+renderJs js = H.script ! (A.type_ "text/javascript") !
+	(A.src $ H.toValue $ prependPath js) $ ""
+
+renderCss :: String -> H.Html
+renderCss css = H.link ! (A.rel "stylesheet") ! (A.type_ "text/css") !
+	(A.href $ H.toValue $ prependPath css)
+
+renderMeta :: [String] -> H.Html
+renderMeta meta = H.script $ H.toHtml $ "JX.Stratcom.mergeData(0, " ++ show meta ++ ");"
